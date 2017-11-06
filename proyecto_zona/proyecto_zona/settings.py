9a98d7f97b700e611ed0626f9c1f1f01edbddd6c
Edvin Daniel <ed_xh@hotmail.com>
2017-11-05 18:33:28 -0600

    Agregando sitios

--- proyecto_zona/proyecto_zona/settings.py
+++ proyecto_zona/proyecto_zona/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'tb&0)((kxyy5mt_gha(($&x%7r2c*=(ni_z^l-!g(ssx1*a=@r'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','edx92.pythonanywhere.com']
 
 
 # Application definition
